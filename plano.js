d8a0358cf370d996a99b06fdb9569f3766aa502c
florek <florek@florkiewicz.me>
2015-10-14 00:01:57 +0200

    Adding, removing, renaming events

--- plano.js
+++ plano.js
@@ -1,22 +1,70 @@
 Events = new Mongo.Collection('events');
 
+var highlight_subject = '';
+
 if (Meteor.isClient) {
 	Meteor.subscribe('events');
 
-	Template.callendar.helpers({
+	Template.body.helpers({
 		options: function() {
 			return {
 				defaultView: 'agendaWeek',
 				minTime : "06:00:00",
 				maxTime : "22:00:00",
 				selectable: true,
+				editable: true,
 				select: function(s, e) {
-					Meteor.call('addEvent', "FOO", s.toDate(), e.toDate(), 2);
+					Meteor.call('addEvent', "FOO", s.toISOString(), e.toISOString(), 2);
 					console.log(s.toDate());
 					console.log(e.toDate());
+				},
+				events: function(start, end, tz, callback) {
+					var events = Events.find().map(function(it) {
+						return {
+							id: it._id,
+							title: it.subject,
+							start: it.start,
+							end: it.end,
+							dow: it.dow
+						}
+					});
+					console.log(events);
+					callback(events);
+				},
+				eventClick: function(event, jsEvent, view) {
+					if(jsEvent.ctrlKey) {
+						console.log("remove: ", event);
+						Meteor.call('removeEvent', event.id);
+					} else {
+							var input = $('<input>', {
+								value: event.title,
+								style: 'position: absolute; left: ' + jsEvent.pageX + 'px; top: ' +jsEvent.pageY + 'px; z-index: 10;'
+							}).keyup(function() {
+								console.log($(this).val());
+								event.title = $(this).val();
+								$('.fc').fullCalendar('updateEvent', event);
+							}).blur(function() {
+								Meteor.call('renameEvent', event.id, event.title);
+								$(this).remove();
+							}).appendTo($('body'));
+
+							var val = input.val();
+							input.val(val);
+					}
+				},
+				eventMouseover: function(event, jsEvent, view) {
+					highlight_subject = event.title;
+					console.log(event);
+				},
+				eventDrop: function(event, _, revertFunc) {
+					Meteor.call('updateEvent', event.id, event.start.toISOString(), event.end.toISOString());
+				},
+				eventResize: function(event, _, revertFunc) {
+					Meteor.call('updateEvent', event.id, event.start.toISOString(), event.end.toISOString());
 				}
 			};
 		},
+			/*
 		events: function() {
 			var fc = $('.fc');
 			return function(start, end, tz, callback) {
@@ -35,13 +83,12 @@ if (Meteor.isClient) {
 				callback(events);
 			}
 		}
-
+		*/
 	});
+
 	Template.body.rendered = function () {
 		var fc = this.$('.fc');
 		this.autorun(function () {
-			//1) trigger event re-rendering when the collection is changed in any way
-			//2) find all, because we've already subscribed to a specific range
 			Events.find();
 			fc.fullCalendar('refetchEvents');
 		});
@@ -49,24 +96,39 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
 	Meteor.publish('events', function(s, e) {
 		return Events.find();
 	});
 
 	Meteor.methods({
 		addEvent: function(subject, start, end, recurrance) {
-			console.log('add', subject);
+			console.log('add', subject, start, end);
 			Events.insert({
 				subject: subject,
 				start: start,
 				end: end,
 				recurrance: recurrance
 			});
+		},
+		removeEvent: function(eid) {
+			console.log('remove ', eid);
+			Events.remove(eid);
+		},
+		updateEvent: function(eid, start, end) {
+			console.log(eid);
+			Events.update(eid, {
+				$set: { 
+					start : start, 
+					end : end 
+				}
+			});
+		},
+		renameEvent: function(eid, name) {
+			Events.update(eid, {
+				$set: { 
+					subject : name 
+				}
+			});
 		}
-	});
-
-	Meteor.startup(function () {
-		// code to run on server at startup
 	});
 }
